f7bff4c13e9bb716f744cdf399df9504d8d817df
angel piña <angelpiviveros@gmail.com>
2022-04-15 19:34:25 -0500

    Se diseñaron las targetas de presentacion de las impresoras

--- resources/views/impresoras.blade.php
+++ resources/views/impresoras.blade.php
@@ -1,16 +1,27 @@
 @extends('welcome')
 
-@section('sesion')
-    <h1>soy la seccion de impresoras</h1>
+@section('sesion')    
+    <div class="grid grid-cols-2 justify-items-center w-full h-auto">
+        @foreach ($impresoras as $item)
 
-    @foreach ($impresoras as $item)
+            <div class="col-span-1 w-1/2 h-auto bg-white m-4 rounded-lg targetaVentaimpresora">
+                <img class="mt-4 imgImpresoras" src="https://www.copyservice.es/WebRoot/StoreES3/Shops/ec9319/MediaGallery/Categories/PRODUCTOS/_0022_BIZHUB_0020_COLOR_0022_/BIZHUB_C458/C458_01.png" 
+                alt="" width="400px" height="400px">
+                <div class="text-center mt-6">
+                    <h3 class="text-3xl font-titulo">${{$item->precio}}</h3>
+                    <h5 class="text-2xl font-titulo m-2"><span>{{$item->marca}} </span>{{$item->modelo}}</h5>
+                </div>
 
-    <h1>{{$item->id}}</h1>
-    <h1>{{$item->modelo}}</h1>
-    <h1>{{$item->marca}}</h1>
-    <h1>{{$item->Descripcion}}</h1>
-    <h1>${{$item->precio}}</h1>
-    <h1>{{$item->stock}}</h1>
-        
-    @endforeach
+                <div class="text-justify font-descrip p-3">
+                    <p>{{$item->Descripcion}} Lorem ipsum dolor sit amet.</p>
+                </div>
+
+                <div class="grid grid-cols-2 w-full">
+                    <button class="m-4 col-span-1 w-auto h-8 bg-blue-400 rounded-lg font-descrip hover:bg-blue-300">Ficha Tecnica</button>
+                    <button class="m-4 col-span-1 w-auto h-8 bg-green-500 rounded-lg font-descrip hover:bg-green-400">Agregar al carrito</button>
+                </div>
+            </div>
+    
+        @endforeach
+    </div>
 @endsection
