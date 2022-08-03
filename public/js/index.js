3d22cdaa7fc24768db7e031dbc75c951d7050344
angel piña <angelpiviveros@gmail.com>
2022-08-02 22:55:48 -0500

    Se completo el formulario de regustro, a un no esta funcional al 100%

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,35 @@
+
+//Funcion que rellena el select del los estados
+let select=document.getElementById('estados');
+let SelectMunicipios=document.getElementById('municipios');
+let municipios=[];
+
+function estados(){
+   fetch('js/estados-municipios.json')
+   .then(respuesta => respuesta.json())
+   .then(respuesta=>{
+      municipios=respuesta
+      Object.entries(respuesta).forEach(([key]) => {
+      let option = document.createElement("option"); 
+      option.value =`${key}`;
+      option.text =`${key}`;
+      select.appendChild(option);
+      
+   })})
+      
+}
+estados();
+// Funcion que busca los municipios al seleccionar un estado
+select.addEventListener('change', function(){
+  SelectMunicipios.innerHTML=`<option>Seleccione un municipio</option>`
+    municipios[select.value].forEach(municipio=>{
+       SelectMunicipios.innerHTML+= `<option value="${municipio}">${municipio}</option>`
+    })
+  
+  
+})
+
+
 /**Bon resposivo que muestra y oculta el menu*/
 const boton=document.querySelector('#navegation');
 const menu=document.querySelector('#menu'); 
@@ -34,4 +66,7 @@ login.addEventListener('submit', e=>{
   textoError.classList.add('hidden');
 })
  
+
+
+
 
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -8,14 +8,22 @@
                 <h2 class="tituloFormularioRegistro">Soluciones En Impresión</h2>
             </div>
             @csrf
-            <input class="inputsRegistro" type="text" name="nombre" placeholder="Nomre(s)">
+            <input class="inputsRegistro" type="text" name="nombre" placeholder="Nombre(s)">
             <input class="inputsRegistro" type="text" name="apellidos" placeholder="Apellidos">
             <input class="inputsRegistro" type="email" name="email" placeholder="Email">
             <input class="inputsRegistro" type="password" name="password" placeholder="Contraseña">
             <input class="inputsRegistro" type="text" name="telefono" placeholder="Teléfono">
-            <input class="inputsRegistro" type="text" name="estado" placeholder="Estado">
-            <input class="inputsRegistro" type="text" name="ciudad" placeholder="Ciudad">
+            <select class="inputsRegistro" name="estados" id="estados">
+                <option>Selecciona un Estado</option>
+            </select>
+            <select class="inputsRegistro" name="municipios" id="municipios">
+                <option>Seleccione un municipio</option>
+            </select>
             <input class="inputsRegistro" type="text" name="cp" placeholder="Codigo Postal">
+            <input class="inputsRegistro" type="text" name="colonia" placeholder="Colonia">
+            <input class="inputsRegistro" type="text" name="calle" placeholder="Calle">
+
+            
             <button class="col-span-4 mt-4 bg-green-500 w-full h-10 rounded-2xl font-descrip" type="submit">Registrarme</button>
         </form>
     </div>
