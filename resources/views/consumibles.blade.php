c844c1864a5f91ceabb761f8ada523ce0214d76f
Angel <angelpivalencia@gmail.com>
2023-05-19 17:51:28 -0600

    Se esta construyecto la sesion de consumibles

--- resources/views/consumibles.blade.php
+++ resources/views/consumibles.blade.php
@@ -1,5 +1,27 @@
 @extends('welcome')
 
 @section('sesion')
-    <h1>soy la seccion de consumibles</h1>
+<div class="grid grid-cols-3 p-4 m-auto mt-10 justify-items-center w-5/6 gap-4 h-auto">
+    @foreach ($consumibles as $item)
+
+        <div class="col-span-1 w-full mt-5 h-auto bg-white rounded-lg targetaVentaimpresora">
+            <img class="mt-4 imgImpresoras p-3" src="{{$item->rutaImg}}" 
+            alt="" width="400px" height="400px">
+            <div class="text-center mt-6">
+                <h3 class="text-3xl font-titulo">{{$item->modelo}}</h3>
+                <h5 class="text-2xl font-titulo m-2"><span>{{$item->marca}} </span>{{$item->modelo}}</h5>
+            </div>
+
+            <div class="text-justify font-descrip p-3">
+                <p>{{$item->Descripcion}} Lorem ipsum dolor sit amet.</p>
+            </div>
+
+            <div class="grid grid-cols-2 w-full">
+                <button class="m-4 col-span-1 w-auto h-8 bg-blue-400 rounded-lg font-descrip hover:bg-blue-300">Ficha Tecnica</button>
+                <button class="m-4 col-span-1 w-auto h-8 bg-green-500 rounded-lg font-descrip hover:bg-green-400">Agregar al carrito</button>
+            </div>
+        </div>
+
+    @endforeach
+</div>
 @endsection
