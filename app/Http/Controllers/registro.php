a36e9d0ab77c8977a33c613be57992e90887b9cf
ANGEL PIÑA VIVEROS <angelpiviveros@gmail.com>
2024-06-08 07:04:01 -0600

    Se esta validando el formulario de registro

--- app/Http/Controllers/registro.php
+++ app/Http/Controllers/registro.php
@@ -8,12 +8,81 @@ use App\Models\User;
 class registro extends Controller{
 
     // Funcion que registra a los usuario 
-    public function store(){
+    public function store(Request $request){     
+        // Validación de los datos del formulario
+        $this->validate($request,[
+            'nombre' => 'required|string|min:3|max:50',
+            'apellidos' => 'required|string|min:3|max:50',
+            'email' => 'required|email',
+            'password' => 'required|string|min:8',
+            'telefono' => ['required','regex:/^\d{10}$/'], ['telefono.required' => 'El campo teléfono es obligatorio.','telefono.regex' => 'El formato del teléfono es inválido.'],
+            'estado' => 'required|string|max:100',
+            'ciudad' => 'required|string|max:100',
+            'cp' => 'required|int|min:5|max:5',
+            'colonia' => 'required|string|min:3|max:100',
+            'calle' => 'required|string|min:3|max:100',
 
-        $user = User::create(request(['nombre','apellidos','email','password','telefono','estado','ciudad','cp','colonia','calle']));
+        ]);
 
-        auth()->login($user);
-        return redirect()->to('/');
+
+        $user = User::create([
+            'nombre'=>$request->nombre,
+            'apellidos'=>$request->apellidos,
+            'email'=>$request->email,
+            'password'=>$request->password,
+            'telefono'=>$request->telefono,
+            'estado'=>$request->estado,
+            'ciudad'=>$request->ciudad,
+            'cp'=>$request->cp,
+            'colonia'=>$request->colonia,
+            'calle'=>$request->calle
+        ]); 
+        
+        return back()->withErrors(['error'=>'¡Ocurrio un error!']);
+
+        // try {
+        //     // Validación de los datos del formulario
+        // $this->validate($request,[
+        //     'nombre' => 'required|string|min:3|max:255',
+        //     'apellidos' => 'required|string|min:3|max:255',
+        //     'email' => 'required|email|unique:email',
+        //     'password' => 'required|string|min:8',
+        //     'telefono' => 'required|numeric|min:10|max:10|unique:telefono',
+        //     'estado' => 'required|string|max:255',
+        //     'ciudad' => 'required|string|max:255',
+        //     'cp' => 'required|integer|min:5|max:5',
+        //     'colonia' => 'required|string|min:3|max:255',
+        //     'calle' => 'required|string|min:3|max:255',
+
+        // ]);
+
+
+        // $user = User::create([
+        //     'nombre'=>$request->nombre,
+        //     'apellidos'=>$request->apellidos,
+        //     'email'=>$request->email,
+        //     'password'=>$request->password,
+        //     'telefono'=>$request->telefono,
+        //     'estado'=>$request->estado,
+        //     'ciudad'=>$request->ciudad,
+        //     'cp'=>$request->cp,
+        //     'colonia'=>$request->colonia,
+        //     'calle'=>$request->calle
+        // ]);
+
+        // } catch (\Exception $e) {
+        //        return back()->withErrors(['registroError'=>'¡Ocurrio un error!']);
+        // }
+        
+
+        // if ($user) {
+        //     auth()->login($user);
+        //     return redirect()->to('/');
+        // }else{
+        //     return back()->with([
+        //         'mensaje','¡Ha ocurrido un error al registrarte, rebisa tus datos.!'
+        //     ]);
+        // }
 
     }
 
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -4,27 +4,47 @@
     <div class="divpadreRegistro">
         <form action="registro" method="post" class="formRegistro">
             <div class="grid grid-cols-1 justify-items-center col-span-4 text-center">
-                <img src="{{asset('img/logo-bien.png')}}" alt="" width="160px" height="160px">
+                <img src="{{ asset('img/logo-bien.png') }}" alt="" width="160px" height="160px">
                 <h2 class="tituloFormularioRegistro">Soluciones En Impresión</h2>
             </div>
             @csrf
-            <input class="inputsRegistro" type="text" name="nombre" placeholder="Nombre(s)">
-            <input class="inputsRegistro" type="text" name="apellidos" placeholder="Apellidos">
-            <input class="inputsRegistro" type="email" name="email" placeholder="Email">
-            <input class="inputsRegistro" type="password" name="password" placeholder="Contraseña">
-            <input class="inputsRegistro" type="text" name="telefono" placeholder="Teléfono">
-            <select class="inputsRegistro" name="estado" id="estados">
+            <input class="inputsRegistro" type="text" name="nombre" placeholder="Nombre(s)" value="{{ old('nombre') }}">
+            <input class="inputsRegistro" type="text" name="apellidos" placeholder="Apellidos"
+                value="{{ old('apellidos') }}">
+            <input class="inputsRegistro" type="email" name="email" placeholder="Email" value="{{ old('email') }}">
+            <input class="inputsRegistro" type="password" name="password" placeholder="Contraseña" value="{{ old('password') }}">
+            <input class="inputsRegistro" type="text" name="telefono" placeholder="Teléfono"
+                value="{{ old('telefono') }}">
+            <select class="inputsRegistro" name="estado" id="estados" value="{{ old('estado') }}">
                 <option>Selecciona un Estado</option>
             </select>
-            <select class="inputsRegistro" name="ciudad" id="municipios">
+            <select class="inputsRegistro" name="ciudad" id="municipios" value="{{ old('ciudad') }}">
                 <option>Seleccione un municipio</option>
             </select>
-            <input class="inputsRegistro" type="text" name="cp" placeholder="Codigo Postal">
-            <input class="inputsRegistro" type="text" name="colonia" placeholder="Colonia">
-            <input class="inputsRegistro" type="text" name="calle" placeholder="Calle">
+            <input class="inputsRegistro" type="text" name="cp" placeholder="Codigo Postal"
+                value="{{ old('cp') }}">
+            <input class="inputsRegistro" type="text" name="colonia" placeholder="Colonia" value="{{ old('colonia') }}">
+            <input class="inputsRegistro" type="text" name="calle" placeholder="Calle" value="{{ old('calle') }}">
 
-            
-            <button class="col-span-4 mt-4 bg-green-500 w-full h-10 rounded-2xl font-descrip" type="submit">Registrarme</button>
+            @if ($errors->any())
+                <ul>
+                    @foreach ($errors->all() as $error)
+                    <li>{{$error}}</li>
+                        
+                    @endforeach
+                </ul>
+                
+            @endif
+
+             {{-- @error('registroError')
+                <div id="usuarioError" class="textoError">
+                    <p>{{ $message }}</p>
+                </div>
+            @enderror  --}}
+
+
+            <button class="col-span-4 mt-4 bg-green-500 w-full h-10 rounded-2xl font-descrip"
+                type="submit">Registrarme</button>
         </form>
     </div>
 @endsection
