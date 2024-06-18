acf50820bef22257b6b05bead26ac25fc71eebab
ANGEL PIÑA VIVEROS <angelpiviveros@gmail.com>
2024-06-17 22:34:37 -0600

    Realicé un if con la validaciones, funciona pero el problema esta cuando valla a mostrar los msj de error.

--- public/js/validaciones.js
+++ public/js/validaciones.js
@@ -1,73 +1,88 @@
 // Validar los campos de inicio de sesión
-let login=document.getElementsByName('login')[0];
-let textoError=document.getElementById('error');
-let textErrorUsuario=document.getElementById('usuarioError');
-let botonLogin=document.getElementById('bottonLogion');
-const expreciones={
-    email: /^[a-zA-Z0-9_.ñÑ+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+let login = document.getElementsByName('login')[0];
+let textoError = document.getElementById('error');
+let textErrorUsuario = document.getElementById('usuarioError');
+let botonLogin = document.getElementById('bottonLogion');
+const expreciones = {
+  email: /^[a-zA-Z0-9_.ñÑ+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  soloString: /^[a-zA-Z]+$/
 }
 //El && evalua que el objeto login no este vacio o sea null
 //Así evitamos que el codigo se detenga y no se ejecuten las otras funciones.
-login && login.addEventListener('input', function(){
-    
-  if(expreciones.email.test(login.email.value)!=true||login.password.value==''){
-    botonLogin.setAttribute('disabled','disabled');
+login && login.addEventListener('input', function () {
+
+  if (expreciones.email.test(login.email.value) != true || login.password.value == '') {
+    botonLogin.setAttribute('disabled', 'disabled');
     botonLogin.classList.remove('buttonLoginEnable')
     botonLogin.classList.add('buttonLogin')
 
-  }else{
+  } else {
     botonLogin.removeAttribute('disabled');
     botonLogin.classList.remove('buttonLogin')
-    botonLogin.classList.add('buttonLoginEnable')    
+    botonLogin.classList.add('buttonLoginEnable')
   }
-    
- 
-  
+
+
+
 });
 
 
- //Quitar error de usuario o contraseña
-  login && login.email.addEventListener('click', ()=>{
-    textoError.classList.add('hidden');
-    textErrorUsuario.classList.add('hidden');
+//Quitar error de usuario o contraseña
+login && login.email.addEventListener('click', () => {
+  textoError.classList.add('hidden');
+  textErrorUsuario.classList.add('hidden');
 
-  })
+})
 
-  login && login.password.addEventListener('click', ()=>{
-   textoError.classList.add('hidden');
-  })
+login && login.password.addEventListener('click', () => {
+  textoError.classList.add('hidden');
+})
 
-  // VALIDACION DE LOS INPUTS DE EL FORMULARIO REGISTRO. 
- let formularioRegistro=document.getElementsByName('registro')[0];
- let buttonReg=document.getElementById('buttonRegistro');
- formularioRegistro.addEventListener('input', function(){
+// VALIDACION DE LOS INPUTS DE EL FORMULARIO REGISTRO. 
+let formularioRegistro = document.getElementsByName('registro')[0];
+let buttonReg = document.getElementById('buttonRegistro');
+
+formularioRegistro.addEventListener('input', function () {
   //Objeto que guarda todos los valores que reciva el formulario de registro.
-  let entradaFormReg={
-    nombre:formularioRegistro.nombre.value.length,
-    apellidos:formularioRegistro.apellidos.value.length,
-    email:formularioRegistro.email.value,
-    password:formularioRegistro.password.value,
-    telefono:formularioRegistro.telefono.value,
-    estado:formularioRegistro.estado.value,
-    ciudad:formularioRegistro.ciudad.value,
-    postal:formularioRegistro.cp.value,
-    colonia:formularioRegistro.colonia.value,
-    calle:formularioRegistro.calle.value
+  let entradaFormReg = {
+    nombre: formularioRegistro.nombre.value,
+    apellidos: formularioRegistro.apellidos.value,
+    email: formularioRegistro.email.value,
+    password: formularioRegistro.password.value,
+    telefono: formularioRegistro.telefono.value,
+    estado: formularioRegistro.estado.value,
+    ciudad: formularioRegistro.ciudad.value,
+    postal: formularioRegistro.cp.value,
+    colonia: formularioRegistro.colonia.value,
+    calle: formularioRegistro.calle.value
   }
 
   //Hay que estudiar cual es la mejor practica para validar cada input de formulario registro
-  
-  if (entradaFormReg.nombre>=4){
+  //Validar existencia, tipo de dato, longitud y duplicación.
+  if (entradaFormReg.nombre.length >= 3 &&
+    entradaFormReg.nombre.length <= 25 &&
+    expreciones.soloString.test(entradaFormReg.nombre) == true &&
+    entradaFormReg.apellidos.length >= 3 &&
+    entradaFormReg.apellidos.length <= 30 &&
+    expreciones.soloString.test(entradaFormReg.apellidos) == true &&
+    entradaFormReg.email.length >= 3 &&
+    entradaFormReg.email.length <= 25 &&
+    expreciones.email.test(entradaFormReg.email)) {
+
     console.log('se bloquea');
     buttonReg.removeAttribute('disabled');
-  }else{
+    buttonReg.classList.remove('buttonRegistro');
+    buttonReg.classList.add('buttonRegistroEnable');
+  } else {
     console.log('se desbloquea');
-    buttonReg.setAttribute('disabled','disabled');
+    buttonReg.setAttribute('disabled', 'disabled');
+    buttonReg.classList.remove('buttonRegistroEnable');
+    buttonReg.classList.add('buttonRegistro');
   }
-  
-      
-      
-   
-   //console.log(formularioRegistro.apellidos.value);
 
- });
+
+
+
+  //console.log(formularioRegistro.apellidos.value);
+
+});
