1cdfec52c8dc1b496243ece9e33372be23522a7c
ANGEL PIÑA VIVEROS <angelpiviveros@gmail.com>
2024-08-10 22:32:49 -0600

    Se creó el archivo formularioRegistro.js, se movio todas las validaciones del formulario registro a ese archivo. El formulario login no funciona esta en revicion.

---
+++ public/js/formularioRegistro.js
@@ -0,0 +1,25 @@
+
+import { validarInputsDeNuevoRegistro } from "./validarInputs.js";
+
+let buttonReg = document.getElementById('buttonRegistro');
+
+let entradaFormReg = {
+  nombre: document.getElementById('regNombre'),
+  apellidos: document.getElementById('regApellidos'),
+  email: document.getElementById('regEmail'),
+
+}
+
+
+entradaFormReg.nombre.addEventListener('input', function () {
+  validarInputsDeNuevoRegistro(entradaFormReg.nombre.value, entradaFormReg.nombre.id);
+})
+
+//VALIDAR EL INPUT DE APELLIDOS.
+entradaFormReg.apellidos.addEventListener('input', function () {
+  validarInputsDeNuevoRegistro(entradaFormReg.apellidos.value, entradaFormReg.apellidos.id);
+})
+
+entradaFormReg.email.addEventListener('input', function () {
+  validarInputsDeNuevoRegistro(entradaFormReg.email.value, entradaFormReg.email.id);
+})
