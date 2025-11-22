c2fd0642a7caa801c4937a3681b2725393bc6962
AngelPV <angelpiviveros@gmail.com>
2025-11-21 22:18:48 -0600

    La sesión del primer servicio ya es responsiva

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,29 +5,29 @@ module.exports = {
 
     extend: {
       // fontSize:{'xs': '.7rem'},
-      screens:{
-        'xs': {'min': '280px', 'max': '640px'},
+      screens: {
+        'xs': { 'min': '280px', 'max': '640px' },
       },
       textColor: {
         'titulos': '#505556',
-        'comentario':'#656363'
+        'comentario': '#656363'
       },
 
-      fontFamily:{
-        'titulo':['"Calistoga"', 'cursive'],
-        'descrip':['"Open Sans"','sans-seri'],
+      fontFamily: {
+        'titulo': ['"Calistoga"', 'cursive'],
+        'descrip': ['"Open Sans"', 'sans-seri'],
 
       },
       backgroundImage: theme => ({
         'fondo': "url('/img/amarillo.svg')",
-       })
-      
+      })
+
     },
   },
   variants: {
     extend: {
       borderWidth: ['hover', 'focus'],
-      borderColor: ['hover','active'],
+      borderColor: ['hover', 'active'],
       transform: ['hover', 'focus'],
       transitionDuration: ['hover', 'focus'],
       display: ['group-focus']
