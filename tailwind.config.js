0db0c83d5848e6ed483aff5bec0da092b8852ccb
AngelPV <angelpiviveros@gmail.com>
2026-02-08 17:01:56 -0600

    WIP: Ajustando los breakpoints, falta correguir que el diseño no se destruya abajo de 640px.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,13 +12,7 @@ module.exports = {
   darkMode: false,
   theme: {
     extend: {
-      screens: {
-        'xs': { 'min': '280px', 'max': '640px' },
-        'sm': '640px',
-        'md': '768px',
-        'lg': '1024px',
-        'xl': '1280px',
-      },
+     
       textColor: {
         'titulos': '#505556',
         'comentario': '#656363'
