cdcee3e004baab0fec95d85d25a841d3e3ac6429
AngelPV <angelpiviveros@gmail.com>
2026-03-23 22:07:56 -0600

    WIP: Se esta trabajando en el diseño de las imagnes para que todas esten parejas.

--- resources/views/impresoras.blade.php
+++ resources/views/impresoras.blade.php
@@ -6,7 +6,7 @@
 
             <div class="contenedorImagen targetaVentaimpresora">
                 <img class="imgImpresoras" src="{{$item->img}}" 
-                alt="" width="400px" height="400px">
+                alt="" >
                 <div class="contenedorPrecio">
                     <h3 class="textoPrecio">${{$item->precio}}</h3>
                     <h5 class="textoModelo"><span>{{$item->marca}} </span>{{$item->modelo}}</h5>
