cf99a4239ee65d86465147346837a008a3f26e10
AngelPV <angelpiviveros@gmail.com>
2026-03-31 22:06:29 -0600

    WIP: Se le dio estructura a los datos guardados en la DB, los estilos son los mismos que la sesión de impresoras.

--- resources/views/consumibles.blade.php
+++ resources/views/consumibles.blade.php
@@ -5,19 +5,19 @@
     @foreach ($consumibles as $item)
 
         <div class="contenedorImagen targetaVentaimpresora">
-            <img class="imgImpresoras" src="{{$item->rutaImg}}" alt="" width="400px" height="400px">
+            <img class="imgImpresoras" src="{{$item->img}}" alt="" width="400px" height="400px">
 
             <div class="contenedorPrecio">
-                <h2 class="textoPrecio">${{$item->presio}}</h2>
+                <h2 class="textoPrecio">${{$item->precio}}</h2>
                 <h3 class="textoModelo">{{$item->modelo}} {{$item->marca}}</h3>
                 
             </div>
 
             <div class="textoDescripcion">
-                <p>{{$item->descripcion}} Lorem ipsum, dolor sit amet consectetur adipisicing elit. Alias, nesciunt. 
-                    Placeat assumenda tenetur eius! Quo, qui. 
-                    Obcaecati illo, officia aliquid temporibus porro, officiis vero animi nisi iusto possimus libero et.</p>
+                <p>{{$item->descripcion}}</p>
             </div>
+
+            <button class="btnAñadirCarrito">Añadir al Carrito</button>
 
         </div>
 
