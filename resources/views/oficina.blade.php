15f136cdcfab7ce50e222158f2cbc954fa349ac3
AngelPV <angelpiviveros@gmail.com>
2026-04-05 16:56:37 -0500

    La sesió de productos de oficina y tecnología quedo lista, con el mismo diseño de tarjetas de ventas que las otras sesiones. De la misma forma se adapta a todas las pantallas.

--- resources/views/oficina.blade.php
+++ resources/views/oficina.blade.php
@@ -1,13 +1,22 @@
 @extends('welcome')
 
 @section('sesion')
+    <div class="cotenedorPadre">
+        @foreach ($productosOficina as $item)
+            <div class="contenedorImagen targetaVentaimpresora">
+                <img class="imgImpresoras" src="{{ $item->img }}" alt="">
+                <div class="contenedorPrecio">
+                    <h3 class="textoPrecio">${{ number_format($item->precio, 2, '.', ',') }}</h3>
+                    <h5 class="textoModelo"><span>{{ $item->marca }} - </span>{{ $item->nombre }}</h5>
+                </div>
 
- @foreach ($productosOficina as $item)
-   {{ $item->nombre }} <br>
-   {{ $item->descripcion }} <br>
-    {{ $item->precio }} <br>
-    {{ $item->stock }} <br>
-    {{ $item->sku }} <br>
- @endforeach
+                <div class="textoDescripcion">
+                    <p>{{ $item->descripcion }}</p>
+                </div>
 
+                <button class="btnAñadirCarrito">Añadir al Carrito</button>
+
+            </div>
+        @endforeach
+    </div>
 @endsection
